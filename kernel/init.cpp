0f1a2b98a7594d6129983834de8bdbb6d0da376b
Monroe Clinton <contact@monroeclinton.com>
2022-07-23 20:44:29 -0400

    kernel: Basic bootstrap

---
+++ kernel/init.cpp
@@ -0,0 +1,10 @@
+extern "C" void init()
+{
+    const char* str = "Hello World!";
+    char* screen = (char*)0xB8000;
+
+    while (*str != 0) {
+        *screen++ = *str++;
+        *screen++ = 15;
+    }
+}
